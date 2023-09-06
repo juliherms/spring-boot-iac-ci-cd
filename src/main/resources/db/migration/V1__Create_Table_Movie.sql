79872a3999b1510843279f2f0c625378d10e972b
Juliherms Vasconcelos <37042672+juliherms@users.noreply.github.com>
2023-09-05 22:32:46 -0300

    Update V1__Create_Table_Movie.sql

--- src/main/resources/db/migration/V1__Create_Table_Movie.sql
+++ src/main/resources/db/migration/V1__Create_Table_Movie.sql
@@ -6,3 +6,4 @@ CREATE TABLE `movie` (
 
 INSERT INTO `movie` (`description`) VALUES ('Batman Begins');
 INSERT INTO `movie` (`description`) VALUES ('Avengers: Infinity War');
+INSERT INTO `movie` (`description`) VALUES ('Guardians of the Galaxy Vol. 3');
